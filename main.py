32a1153dcda74eb30035f7b5a9abf7ea426d601d
이현호 <hyunho1027@khu.ac.kr>
2020-02-04 23:40:43 +0900

    main update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from env import Env
 from dqn import DQN
+import traceback
 
 agent = DQN()
 env = Env()
@@ -17,17 +18,17 @@ try:
             s = ns
             score += r
         
-        if ep > 100:
+        if True:#ep > 100:
             loss = agent.train()
             print(f"{ep} Epi / Loss : {loss} / Score : {score+100}")
 
-            if not ep%2 and loss != None:
+            if not ep%2 and loss is not None:
                 agent.update_target()
-            if not ep%2 and loss != None:
+            if not ep%2 and loss is not None:
                 agent.save("./models/")
         else:
             print(f"{ep} Epi / Score : {score+100}")
 
 except Exception as e:
-    print(e)
+    traceback.print_exc()
     env.close()
