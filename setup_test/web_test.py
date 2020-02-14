2709826a75ba115626f1c26c7a04d6d588bfd4b8
이현호 <hyunho1027@khu.ac.kr>
2020-02-14 16:12:06 +0900

    erased

--- setup_test/web_test.py
+++
@@ -1,4 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("http://www.trex-game.skipser.com/")
