aebf75e44fa9acc535f733e888f227c45eb962bb
이현호 <hyunho1027@khu.ac.kr>
2020-08-28 20:22:56 +0900

    erase done_mem

--- env.py
+++ env.py
@@ -6,7 +6,6 @@ import webbrowser
 class Env:
     def __init__(self):
         print("LET\'S START!")
-        self.done_mem = True
         self.episode = -1
         self.refresh = 10
         self.remember = None
@@ -14,19 +13,21 @@ class Env:
         self.target_size = (128,64)
 
         # Optimized 1920*1080
-        self.state_region = (self.size[0]//3, self.size[1]//6, self.size[0]//3, self.size[1]//6)
-        self.done_region = ((self.size[0]//10)*9, (self.size[1]//10)*9, self.size[0]//10, self.size[1]//10)
+        self.state_region = (self.size[0]//3, self.size[1]//6,
+                             self.size[0]//3, self.size[1]//6)
+        self.done_region = ((self.size[0]//10)*9, (self.size[1]//10)*9,
+                             self.size[0]//10   ,  self.size[1]//10)
 
         # Open T-Rex Game.
         webbrowser.open("http://www.trex-game.skipser.com/")
         time.sleep(3)
+
         # Full Screen mode
         pag.press('f11')
         time.sleep(1)
 
     def reset(self):
         self.episode += 1
-        self.done_mem=True
         if self.episode%self.refresh == 0:
             pag.press('f5')
             time.sleep(1)
@@ -50,11 +51,7 @@ class Env:
         screen = np.array(screen)
         # Check the Bottom of Screen
         done = (screen<128).any()
-        if self.done_mem == False and done == True:
-            return True
-        else:
-            self.done_mem = done
-            return False
+        return done
 
     def step(self, action):
         key = 'up' if action == 1 else 'down'
